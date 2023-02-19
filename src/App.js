39a3f86bb5b30ee14440b2237b211a3485ae0561
Bnop1146 <79976803+Bnop1146@users.noreply.github.com>
2023-02-19 21:47:57 +0100

    Final design and function of the clock

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+
+function DigitalClock() {
+  const [time, setTime] = useState(new Date());
+  const [displayClock, setDisplayClock] = useState(false);
+
+  useEffect(() => {
+    let intervalID;
+
+    if (displayClock) {
+      intervalID = setInterval(() => setTime(new Date()), 1000);
+    }
+
+    return () => clearInterval(intervalID);
+  }, [displayClock]);
+
+  const handleBackgroundColorChange = () => {
+    const colors = ['yellow', 'orange', 'red', 'purple', 'blue', 'green'];
+    const newColor = colors[Math.floor(Math.random() * colors.length)];
+    document.body.style.backgroundColor = newColor;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="digital-clock-container">
+      <button className="button" onClick={() => setDisplayClock(!displayClock)}>Toggle Clock</button>
+      <button className="button" onClick={handleBackgroundColorChange}>Change Background Color</button>
+      {displayClock && (
+        <div className="clock-container">
+          <h2>{time.toLocaleTimeString([], {hour12: false, hour: '2-digit', minute:'2-digit'})}</h2>
+        </div>
+      )}
     </div>
   );
 }
 
-export default App;
+export default DigitalClock;
